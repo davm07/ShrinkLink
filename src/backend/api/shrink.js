6fe394719e136aaa8c699364440f8695977bf4ce
Diego Vasquez <122320757+davm07@users.noreply.github.com>
2025-02-10 22:19:47 +0100

    Update files to correct path

--- src/backend/api/shrink.js
+++ src/backend/api/shrink.js
@@ -1,4 +1,4 @@
-import connectDB from '../lib/db';
+import connectDB from '../lib/db.js';
 import { createShortUrl } from '../controllers/urlController';
 
 export default async function handler(req, res) {
--- src/backend/api/urls.js
+++ src/backend/api/urls.js
@@ -1,4 +1,4 @@
-import connectDB from '../lib/db';
+import connectDB from '../lib/db.js';
 import { getUserUrls } from '../controllers/urlController';
 
 export default async function handler(req, res) {
